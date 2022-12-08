0578b9ae01defc891d5ee2aa724348f1cfc09a68
osteise <oskarmellgren@live.se>
2022-12-08 22:31:35 +0100

    took back the seperate session stat script

--- program.js
+++ program.js
@@ -104,63 +104,38 @@ if (acdc > mj && bruce && abba && elvis && cash && mc && bob && elton && queen &
 else if (cash > mj && bruce && abba && elvis && acdc && mc && bob && elton && queen && beatles) {
     img.src = "https://i.guim.co.uk/img/static/sys-images/Guardian/About/General/2012/2/28/1330447418243/I-was-evil.-I-really-was--007.jpg?width=465&quality=85&dpr=1&s=none";
 }
-else if (abba > mj || bruce || cash || elvis || acdc || mc || bob || elton || queen || beatles) {
+else if (abba > mj && bruce && cash && elvis && acdc && mc && bob && elton && queen && beatles) {
     img.src = "https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3A68cb1de8-cbdd-11e8-8d0b-a6539b949662?source=ig";
 }
-else if (elton > mj || bruce || abba || elvis || acdc || mc || bob || cash || queen || beatles) {
+else if (elton > mj && bruce && abba && elvis && acdc && mc && bob && cash && queen && beatles) {
     img.src = "https://static-cdn.sr.se/images/2794/c4d9a082-b2a8-412b-8d3e-5844e3f862c4.jpg";
 }
-else if (mj > bruce || cash || abba || elvis || acdc || mc || bob || elton || queen || beatles) {
+else if (mj > bruce && cash && abba && elvis && acdc && mc && bob && elton && queen && beatles) {
     img.src = "https://image.newyork.se/wp-content/uploads/2022/02/MJ-The-Michael-Jackson-Musical-on-Broadway-Tickets-MJ-and-Cast.jpg";
 }
-else if (bruce > acdc || cash || abba || elvis || mc || bob || elton || mj || queen || beatles) {
+else if (bruce > acdc && cash && abba && elvis && mc && bob && elton && mj && queen && beatles) {
     img.src = "https://static01.nyt.com/images/2021/12/17/arts/15culture-springsteen/15culture-springsteen-videoSixteenByNine3000-v2.jpg";
 }
-else if (bob > mj || bruce || abba || elvis || acdc || mc || cash || elton || queen || beatles) {
+else if (bob > mj && bruce && abba && elvis && acdc && mc && cash && elton && queen && beatles) {
     img.src = "https://www.rollingstone.com/wp-content/uploads/2020/01/Bob-Marley-Lead.jpg?w=1581&h=1054&crop=1";
 }
-else if (beatles > mj || bruce || abba || elvis || acdc || mc || bob || elton || queen || cash) {
+else if (beatles > mj && bruce && abba && elvis && acdc && mc && bob && elton && queen && cash) {
     img.src = "https://svd.vgc.no/v2/images/81e63073-137a-45ba-93b3-e8e70a1b9736?h=560&q=80&upscale=true&w=995&s=c8dd41c0e57da0a82faeeab43db3b43ed8428860";
 }
-else if (mc > mj || bruce || abba || elvis || acdc || cash || bob || elton || queen || beatles) {
+else if (mc > mj && bruce && abba && elvis && acdc && cash && bob && elton && queen && beatles) {
     img.src = "https://www.udiscovermusic.com/wp-content/uploads/2020/11/Mo%CC%88tley-Cru%CC%88e-GettyImages-1202277301-1000x600.jpg";
 }
-else if (elvis > mj || bruce || abba || cash || acdc || mc || bob || elton || queen || beatles) {
+else if (elvis > mj && bruce && abba && cash && acdc && mc && bob && elton && queen && beatles) {
     img.src = "https://gaffa.blob.core.windows.net/gaffa-media/media/2503eabc-ac0c-44c8-8a90-14390cf28a2b/ElvisFoljerITupacsFotspar-default.webp";
 }
-else if (queen > mj || bruce || abba || elvis || acdc || mc || bob || elton || cash || beatles) {
+else if (queen > mj && bruce && abba && elvis && acdc && mc && bob && elton && cash && beatles) {
     img.src = "https://www.fractiondiscs.se/wp-content/uploads/2018/01/queen.jpg";
-}
-
-// create a function so that this only works if the session stat page is loaded
-function setImage() {
-    // create an if statement to return if the page is the wrong one
-    if (location.href === 'index.html') {
-        return;
-    }
-    else if (location.href === 'contact.html') {
-        return;
-    }
-    else if (location-href === 'info.html') {
-        return;
-    }
-// target the session stat image
-var img = document.querySelector("img");
-// output the total score on the session stats page
-document.getElementById("totalStat").innerHTML = "Your total stat is: " + total;
-// output the last score on the session stats page
-document.getElementById("lastScore").innerHTML = "Your latest score is: " + lastScore;
-// output the highscore on the session stats page
-document.getElementById("highscoreText").innerHTML = "Your highscore is: " + highscore;
-// run the function specific to the session stat page
-imgOutput();
-getTotal();
-getLastScore();
-getHighscore();
 }
 }
 // imgOutput();
 
+var total;
+var img;
 // get the total score from the program by creating a function
 function getTotal() {
     // get the total score from the local storage
@@ -199,3 +174,33 @@ function getHighscore() {
 // run the program
 // getHighscore();
 
+// create a function so that this only works if the session stat page is loaded
+function statPage() {
+    // create an if statement to return if the page is the wrong one
+    if (location.href === 'index.html') {
+        return;
+    }
+    else if (location.href === 'contact.html') {
+        return;
+    }
+    else if (location.href === 'info.html') {
+        return;
+    }
+    // else if (location.href === 'session-stats.html') {
+    // target the session stat image
+var img = document.querySelector("img");
+// output the total score on the session stats page
+document.getElementById("totalStat").innerHTML = "Your total stat is: " + total;
+// output the last score on the session stats page
+document.getElementById("lastScore").innerHTML = "Your latest score is: " + lastScore;
+// output the highscore on the session stats page
+document.getElementById("highscoreText").innerHTML = "Your highscore is: " + highscore;
+// run the function specific to the session stat page
+console.log("HEJ");
+imgOutput();
+}
+// }
+getTotal();
+getLastScore();
+getHighscore();
+statPage();
